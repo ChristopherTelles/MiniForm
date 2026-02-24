abead9ed23bd6a24c1962d25b51b588ca24693a7
Christopher Teles <teleschristopher@gmail.com>
2026-02-24 20:15:26 -0300

    feat(auth): implement password toggle visibility and input length constraint (4-6)

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,29 @@
 let nome = document.getElementById("nome");
 let senha = document.getElementById("senha");
 let btn = document.getElementById("btn");
+let btn_olho = document.getElementById("btn-olho");
 let erro = document.getElementById("erro");
 // console.log(nome.value, senha.value);
 
+btn.addEventListener("click", () => {
+  erro.textContent =
+    nome.value.trim() === "" || senha.value.trim() === ""
+      ? "Preencha todos os campos"
+      : "";
 
-btn.addEventListener("click",() =>{
-    erro.textContent = nome.value.trim() === "" || senha.value.trim() === ""
-    ? "Preencha todos os campos"
-    :"";
+  if (erro.textContent !== "") return;
 
-    if (erro.textContent !== "") return;
+  console.log(`Nome: ${nome.value} \nSenha: ${senha.value}`);
+});
 
-    console.log(`Nome: ${nome.value} \nSenha: ${senha.value}`);
+btn_olho.addEventListener("click", function () {
+  if (senha.type === "password") {
+    senha.type = "text";
+    btn_olho.innerHTML =
+      '<img class="olhos" src="assets/icon/olho-aberto.png" alt="" />';
+  } else {
+    senha.type = "password";
+    btn_olho.innerHTML =
+      '<img class="olhos" src="assets/icon/olho-fechado.png" alt="" />';
+  }
 });
